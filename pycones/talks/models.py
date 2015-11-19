cde5e293d9c5ba90aeaa0df403d9baa7fb939a24
MiguelSR <miguelsr87@gmail.com>
2015-11-19 17:22:27 +0100

    Ups!

--- pycones/talks/models.py
+++ pycones/talks/models.py
@@ -7,7 +7,8 @@ from authors.models import Author
 class Talk(models.Model):
     name = models.CharField(max_length=256, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    authors = models.ManyToManyField(Author, related_name='talks') 
+    authors = models.ManyToManyField(Author, related_name='talks',
+                                     null=True, blank=True) 
 
     def __unicode__(self):
         return self.name
